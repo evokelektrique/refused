184ffa5c3ca183d98832728dccf18dc5f54859ef
EVOKE <evoke.lektrique@gmail.com>
2021-10-11 15:43:56 +0330

    Code refactor

--- src/resources/scripts/popup.js
+++ src/resources/scripts/popup.js
@@ -21,47 +21,50 @@ async function get_power_status(db) {
 
 // Initialize Configuration & Display
 (async () => {
-  const current_tab = await Helper.get_current_tab()
 
-  // Set up current tab url
-  const element_current_tab_url = document.getElementById("current_tab_url")
-  element_current_tab_url.innerHTML = new URL(current_tab.url).host
+  // Elements
+  const element_current_tab_url   = document.getElementById("current_tab_url")
+  const element_current_power     = document.getElementById("power_button")
+  const element_current_status    = document.getElementById("status")
+  const element_block_information = document.getElementById("block_information")
 
   const lang_status_on  = browser.i18n.getMessage("status_on")
   const lang_status_off = browser.i18n.getMessage("status_off")
-  const element_status  = document.getElementById("status")
 
+  // Set up current tab url
+  const current_tab = await Helper.get_current_tab()
+  element_current_tab_url.innerHTML = new URL(current_tab.url).host
   // Get power on/off switch status and add class to its element
   new SettingsDatabase().open().then(async db => {
 
     const status       = await get_power_status(db)
     const total_blocks = await get_total_blocks(db)
 
-    const power_element = document.getElementById("power_button")
     if(status) {
-      power_element.classList.add("power_on")
-      document.getElementById("block_information").classList.add("visible")
+      element_current_power.classList.add("power_on")
+      element_block_information.classList.add("visible")
     } else {
-      power_element.classList.add("power_off")
-      document.getElementById("block_information").classList.remove("visible")
+      element_current_power.classList.add("power_off")
+      element_block_information.classList.remove("visible")
     }
 
-    element_status.innerHTML = lang_status_off
+    element_current_status.innerHTML = lang_status_off
 
     if(status) {
-      element_status.innerHTML = lang_status_on
+      element_current_status.innerHTML = lang_status_on
     }
   })
 
   // Power Button Listener
-  document.getElementById("power_button").addEventListener("click", async e => {
+  element_current_power.addEventListener("click", async e => {
     e.preventDefault()
     e.target.classList.toggle("power_on")
-    document.getElementById("block_information").classList.toggle("visible")
-    if(element_status.innerHTML === lang_status_on) {
-      element_status.innerHTML = lang_status_off
+    element_block_information.classList.toggle("visible")
+
+    if(element_current_status.innerHTML === lang_status_on) {
+      element_current_status.innerHTML = lang_status_off
     } else {
-      element_status.innerHTML = lang_status_on
+      element_current_status.innerHTML = lang_status_on
     }
     
     const sender = await browser.runtime.sendMessage("toggle_status")
