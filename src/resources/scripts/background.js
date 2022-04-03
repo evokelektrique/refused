4cb155f8b3adbe07d7014dc493a2d6b80eb8282e
EVOKE <evoke.lektrique@gmail.com>
2022-04-04 00:22:04 +0430

    Clean up and Move messages listener

--- src/resources/scripts/background.js
+++ src/resources/scripts/background.js
@@ -1,5 +1,6 @@
 import { SettingsDatabase } from "./databases/settings"
 import { SelectorDatabase } from "./databases/selector"
+import { MessageListener } from "./message_listener"
 import { FilterDatabase } from "./databases/filter"
 import { TabManager } from "./tab_manager"
 import { BlockerV1 } from "./blocker_v1"
@@ -16,9 +17,8 @@ const tab_manager = new TabManager()
 new SettingsDatabase().set_settings()
 
 // Initialize alarms
-const alarm_options = { periodInMinutes: 1 }
-browser.alarms.create(constants.alarm_selectors, alarm_options);
-browser.alarms.create(constants.alarm_filters, alarm_options);
+browser.alarms.create(constants.alarm_selectors, constants.alarm_options);
+browser.alarms.create(constants.alarm_filters, constants.alarm_options);
 browser.alarms.onAlarm.addListener(Refused.handle_alarms);
 
 // Handle install or updates
@@ -39,50 +39,8 @@ refused.start()
 
 // Open communication port
 browser.runtime.onConnect.addListener(port => {
-  port.onMessage.addListener(msg => console.log(msg))
+  port.onMessage.addListener(msg => null)
 })
 
 // Listener
-browser.runtime.onMessage.addListener(async message => {
-  let result
-
-  switch(message.type) {
-    case constants.get_selectors:
-      return await new SelectorDatabase().get_selectors(message.domain)
-      break
-
-    case constants.hide_element:
-      break
-
-    case constants.toggle_status:
-      // Get status
-      result = await Helper.get_extension_status()
-
-      // Update status
-      const db = await new SettingsDatabase().open()
-      await db.settings.update(result.id, { value: !result.status })
-
-      // Toggle blocker
-      if(!result.status) {
-        refused.start()
-      } else {
-        refused.stop()
-      }
-
-      return Promise.resolve({ status: !result.status })
-      break
-
-    case constants.get_status:
-      // Get status
-      result = await Helper.get_extension_status()
-
-      return Promise.resolve(result)
-      break
-
-    default:
-      console.log("Unknown message", message)
-      break
-  }
-
-  return true
-})
+browser.runtime.onMessage.addListener((message) => MessageListener(refused, message))
